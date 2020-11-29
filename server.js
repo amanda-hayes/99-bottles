55a40d5118f729249ee6e139914dc64dda7077ef
Amanda Hayes <amandahayes@amandas-mbp.lan>
2020-11-28 17:27:29 -0800

    Tip Calculator express application created.

--- server.js
+++ server.js
@@ -18,4 +18,18 @@ app.get('/greeting/:name', (req, res) => {
     res.send(`What up ${req.params.name}!`)
 });
 
+app.get('/add/:num1/:num2', (req, res) => {
+    const num1 = parseInt(req.params.num1);
+    const num2 = parseInt(req.params.num2);
+    const sum = num1 + num2;
+    res.send(`<h1>The answer is ${sum}</h1>`)
+});
+
+app.get('/tip/:sum1/:sum2', (req, res) => {
+    const sum1 = parseInt(req.params.sum1);
+    const sum2 = parseInt(req.params.sum2);
+    const sum3 = (sum1 * sum2)/100;
+    res.send(`<h1>The answer is ${sum3}</h1>`)
+});
+
 app.listen(3000);
